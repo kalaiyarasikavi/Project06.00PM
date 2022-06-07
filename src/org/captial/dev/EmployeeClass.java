c9b25aa95804f1f7fe760f7b21264ba194fc047d
kalaiyarasi <kalaiyarasikgc@gmail.com>
2022-06-07 14:58:34 +0530

    hi

---
+++ src/org/captial/dev/EmployeeClass.java
@@ -0,0 +1,29 @@
+package org.captial.dev;
+
+public class EmployeeClass {
+	
+	private void getEmployeeId() {
+      System.out.println("123");
+	}
+	
+	private void getEmployeeName() {
+		System.out.println("Uthra");
+	}
+	
+	private void getEmployeeAddress() {
+      System.out.println("Tanmbaram");
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		//ClassName objectName = new ClassName();
+		EmployeeClass ec=new EmployeeClass();
+		//objectName.methodName
+		ec.getEmployeeId();
+		ec.getEmployeeName();
+		ec.getEmployeeAddress();
+	}
+
+}
+
